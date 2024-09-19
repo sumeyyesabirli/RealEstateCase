86dc20e3d0bea466ad70d9ddcaa164020e750c8d
Sumeyye <sumeyyesabirli@gmail.com>
2024-09-19 18:20:12 +0300

    Validasyon ekledim

---
+++ RealEstateCase.Business/Futures/Commands/Property/DeleteProperty/Validation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace RealEstateCase.Business.Futures.Commands.Property.DeleteProperty
+{
+    public class Validation : AbstractValidator<DeletePropertyCommandRequestModel>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.PropertyId)
+                .NotEmpty().WithMessage("Emlak ID'si boş olamaz")
+                .GreaterThan(0).WithMessage("Emlak ID'si sıfırdan büyük olmalıdır");
+        }
+    }
+}
---
+++ RealEstateCase.Business/Futures/Commands/Property/SetAdvertisementStatus/Validation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using RealEstateCase.Business.Futures.Commands.Property.ApproveProperty;
+
+namespace RealEstateCase.Business.Futures.Commands.Property.SetAdvertisementStatus
+{
+    public class Validation : AbstractValidator<SetAdvertisementStatusCommandRequestModel>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.PropertyId)
+                .NotEmpty().WithMessage("Emlak ID'si boş olamaz")
+                .GreaterThan(0).WithMessage("Emlak ID'si sıfırdan büyük olmalıdır");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Geçersiz ilan durumu");
+        }
+    }
+}
